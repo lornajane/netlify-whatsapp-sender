61ca699889ce27828ee7be3e2cf0d0c0baa5ddca
Lorna Jane Mitchell <lorna@lornajane.net>
2020-04-30 14:48:55 +0100

    Add env vars for local, and send message when hook fires

--- functions/send/send.js
+++ functions/send/send.js
@@ -1,6 +1,42 @@
+const Nexmo = require('nexmo')
+require('dotenv').config()
+
 exports.handler = async (event, context) => {
   try {
     const subject = event.queryStringParameters.name || 'World'
+    const body = event.body
+    console.log(body)
+    const data = JSON.parse(body)
+    console.log(data)
+
+    const nexmo = new Nexmo({
+      apiKey: process.env.NEXMO_API_KEY,
+      apiSecret: process.env.NEXMO_API_SECRET,
+      applicationId: process.env.NEXMO_APPLICATION_ID,
+      privateKey: Buffer.from(process.env.NEXMO_APPLICATION_PRIVATE_KEY.replace(/\\n/g, "\n"), 'utf-8')
+    }, {
+      apiHost: process.env.API_HOST
+    })
+
+    nexmo.channel.send(
+      { "type": "whatsapp", "number": process.env.TO_NUMBER },
+      { "type": "whatsapp", "number": process.env.WHATSAPP_NUMBER },
+      {
+        "content": {
+          "type": "text",
+          "text": "This is a WhatsApp Message text message sent using the Messages API"
+        }
+      },
+      (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(data.message_uuid);
+        }
+      }
+    );
+
+
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `Greetings ${subject}` })
